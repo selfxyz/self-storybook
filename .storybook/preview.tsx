22488b92b8df2549f7734a52dde685e64f167478
Justin Hernandez <justin.hernandez@self.xyz>
2025-10-13 20:41:39 -0700

    fix preview

---
+++ .storybook/preview.tsx
@@ -0,0 +1,28 @@
+import type { Preview } from '@storybook/react';
+import React from 'react';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        order: ['React Native', ['Button', 'Header', 'Page']],
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '3em' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
+
